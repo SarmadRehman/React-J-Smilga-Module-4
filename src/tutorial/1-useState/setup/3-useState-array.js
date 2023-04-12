786f60cc232a589ee6e296aba3abfd598bed0a05
Sarmad Rehman <sarmadr02@gmail.com>
2023-04-12 08:29:22 +0500

    completed upto 3-useState-array

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { data } from '../../../data';
-
+import React from "react";
+import { data } from "../../../data";
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  //people is now on default having the array of 'data'
+  const removeItem = (id) => {
+    // this will filter out the array for the exception of given id
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            {/* arrow function so that to trigger when clicked */}
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      {/* setting the state of 'people' to an empty array, clearing everything */}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
