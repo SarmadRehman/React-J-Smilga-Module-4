2a6cb53261da70c2b2e2e9996ba7577f68cd6922
Sarmad Rehman <sarmadr02@gmail.com>
2023-04-13 06:38:40 +0500

    The alterative way for multiple state values

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -6,10 +6,16 @@ const UseStateObj = () => {
     age: 24,
     message: "random message",
   });
+  //commented the code for the alterative way for multiple state values
+  // const [name,setName] = useState('peter')
+  // const [age,setAge] = useState(24)
+  // const [message,setMessage] = useState('random message')
 
   const changeMessage = () => {
     setPerson({ ...person, message: "hello world" });
+    // setMessage('hello world')
   };
+
   return (
     <>
       <h3>{person.name}</h3>
