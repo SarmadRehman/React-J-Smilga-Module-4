9ff5f74f795a9ea327613dd9367ba752a13a4bee
Sarmad Rehman <sarmadr02@gmail.com>
2023-05-18 17:12:32 +0500

    Almost done with the learning not with all the code.Nearly closed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/4-useState-object";
+import Setup from "./tutorial/4-forms/final/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
